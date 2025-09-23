2656143c42057b382163f26abd594d7d92b247c1
fernando j <fer.j.doc@gmail.com>
2025-09-23 20:08:56 -0300

    feat: inclusao de tyeps de retorno das apis.

---
+++ src/api/types.ts
@@ -0,0 +1,84 @@
+
+type thumbnailValues = {
+    url: string,
+    width: number,
+    height: number
+}
+
+type thumbnails = {
+    "default"?: thumbnailValues,
+    "medium"?: thumbnailValues,
+    "high"?: thumbnailValues,
+    "standard"?: thumbnailValues,
+    "maxres"?: thumbnailValues,
+}
+
+export type ResponseVideo = {
+    "kind": string,
+    "etag": string,
+    "items": [
+        {
+            "kind": string,
+            "etag": string,
+            "id": string,
+            "snippet": {
+                "publishedAt": Date | string,
+                "channelId": string,
+                "title": string,
+                "description": string,
+                "thumbnails": thumbnails,
+                "channelTitle": string,
+                "tags": string[],
+                "categoryId": string | number,
+                "liveBroadcastContent": string,
+                "defaultLanguage": string,
+                "localized": {
+                    "title": string,
+                    "description": string
+                },
+                "defaultAudioLanguage": string
+            },
+            "contentDetails": {
+                "duration": string,
+                "dimension": string,
+                "definition": string,
+                "caption": string,
+                "licensedContent": boolean,
+                "contentRating": object,
+                "projection": string
+            },
+            "statistics": {
+                "viewCount": string | number,
+                "likeCount": string | number,
+                "favoriteCount": string | number,
+                "commentCount": string | number
+            }
+        }
+    ],
+    "pageInfo": {
+        "totalResults": number,
+        "resultsPerPage": number
+    }
+}
+
+
+type itemSearch = {
+    "kind": string,
+    "etag": string,
+    "id": {
+        "kind": string,
+        "videoId": string
+    }
+}
+
+export type ResponseSearch = {
+    "kind": string,
+    "etag": string,
+    "nextPageToken": string,
+    "regionCode": string,
+    "pageInfo": {
+        "totalResults": number,
+        "resultsPerPage": number
+    },
+    "items": itemSearch[]
+}
