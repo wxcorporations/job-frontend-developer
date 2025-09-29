ed4aa05b6645e1afb11ebfb4971ead9897e111f5
fernando j <fer.j.doc@gmail.com>
2025-09-29 14:55:12 -0300

    feat: ajustando component inputSearch
    definindo prop search
    - prop possibilita passar um funçao de callback.

--- src/components/InputSearch.tsx
+++ src/components/InputSearch.tsx
@@ -1,16 +1,80 @@
+import React, { HtmlHTMLAttributes, KeyboardEvent } from 'react'
 import './InputSearch.scss'
+import { useRef, useEffect } from 'react'
+
+
+class InputField {
+    el: HTMLInputElement;
+
+    constructor(element: HTMLInputElement) {
+        this.el = element;
+    }
+
+    get value() {
+        return this.el.value
+    }
+
+    clear() {
+        this.el.value = ''
+    }
+
+    block() {
+        this.el.disabled = true
+        this.el.parentElement?.classList.add('is-disable')
+    }
+
+    active() {
+        this.el.disabled = false
+        this.el.parentElement?.classList.remove('is-disable')
+    }
+}
+
 
 export default function InputSearch(props: any) {
-    const msg = (e: any) => {
-        if (e.key === 'Enter') {
-            console.log(e.target.value)
-            e.target.value = ''
+    let input: InputField
+    const search = useRef<HTMLInputElement>(null)
+
+    useEffect(() => {
+        input = new InputField(search.current!)
+    }, [])
+
+    const validateInput = () => {
+        return !(!props.search || typeof props.search !== 'function')
+    }
+
+    const handleClick = () => {
+        if (!validateInput()) return null
+
+        props.search(input.value)
+        input.clear()
+    }
+
+    const handleKeyEnter = (event: any) => {
+        if (!validateInput()) return null
+
+        if (event.key === 'Enter') {
+            props.search(input.value)
+            input.clear()
+        }
+    }
+
+    const handleKeyEnterButton = (event: any) => {
+        if (!validateInput()) return null
+
+        if (event.key === 'Enter') {
+            props.search(input.value)
+            input.clear()
         }
     }
 
     return (
         <>
-            <input className="input-search" placeholder='[ enter ] Para buscar' type="text"  onKeyDown={msg}/>
+            <div className='field-search' >
+                <input tabIndex={1} id="input-search" ref={search} className="field-search__input" placeholder='[ enter ] Para buscar' type="text" onKeyDown={handleKeyEnter} />
+                <div className="field-search__button" role="button" aria-details='button search' tabIndex={2} onClick={handleClick} onKeyDown={handleKeyEnterButton}>
+                    <i className="bi bi-search"></i>
+                </div>
+            </div>
         </>
-    )    
+    )
 }
