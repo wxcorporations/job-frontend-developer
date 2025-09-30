866ec76b5bb18caa6c7794b20c561aeeb3b09ac7
fernando j <fer.j.doc@gmail.com>
2025-09-30 00:23:10 -0300

    fix: ajuste menu

--- src/components/layout/MenuBar.tsx
+++ src/components/layout/MenuBar.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import './MenuBar.scss'
 
 type props = {
-    children: React.ReactNode
+    children?: React.ReactNode
 }
 
 export default function MenuBar(props: props) {
@@ -11,10 +11,10 @@ export default function MenuBar(props: props) {
             <header className="menu-bar">
                 <div className="container">
                     <div className="menu-bar__icon">
-                        <img src="" alt="" width={100} height={50}/>
+                        <img src="#" alt="" width={100} height={50}/>
                     </div>
                     <div className="menu-bar__content">
-                        {props.children}
+                        {props.children && props.children}
                     </div>
                 </div>
             </header>
