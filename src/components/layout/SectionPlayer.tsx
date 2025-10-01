083f9c9c1217eb6d98621dee964f5886020db1c3
fernando j <fer.j.doc@gmail.com>
2025-10-01 18:24:46 -0300

    feat: incluindo store em section player

--- src/components/layout/SectionPlayer.tsx
+++ src/components/layout/SectionPlayer.tsx
@@ -1,12 +1,17 @@
 import { useMemo, useState } from 'react'
 import YoutubeEmbed from '../YoutubeEmbed'
 import ListVideos from '../ListVideos'
+import { useSelector } from 'react-redux'
+
 import './SectionPlayer.scss'
 
 export default function SectionPlayer(props: any) {
     const [video, setVideo] = useState(props.videoId)
-    const [title, setTitle] = useState(props.title)
-    const [description, setDescription] = useState(props.description)
+
+    const description = useSelector((state: any) => state.video.play.description)
+    const channel = useSelector((state: any) => state.video.play.channel)
+    const title = useSelector((state: any) => state.video.play.title)
+    const id = useSelector((state: any) => state.video.play.id)
 
     const embedYoutubeMemo = useMemo(() => {
         return (
@@ -21,12 +26,17 @@ export default function SectionPlayer(props: any) {
         )
     }, [video, title, description]);
 
-
     return (
         <>
             <div className="section-player">
-                <div className="section-player__emded">
-                    {embedYoutubeMemo}
+                <div className="section-player__embed">
+                    <YoutubeEmbed id={id} />
+
+                    <div className='p-2'>
+                        <h1 className='mt-2'>{title}</h1>
+                        <p className='mb-2'>{channel}</p>
+                        <p className='text-secondary'>{description}</p>
+                    </div>
                 </div>
                 <div className="section-player__content">
                     <ListVideos />
