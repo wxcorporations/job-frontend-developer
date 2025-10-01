2339cf5668862b5e9de417f61b7207510845b48f
fernando j <fer.j.doc@gmail.com>
2025-10-01 13:08:34 -0300

    feat: inclusao de icone e label menu-bar

--- src/components/layout/MenuBar.tsx
+++ src/components/layout/MenuBar.tsx
@@ -11,7 +11,8 @@ export default function MenuBar(props: props) {
             <header className="menu-bar">
                 <div className="container">
                     <div className="menu-bar__icon">
-                        <img src="#" alt="" width={100} height={50}/>
+                        <img className="menu-bar__icon-img" src="/assets/octocat.png" alt="imagem de um gato com tentaculos de polvo segurando o icone do youtube" width={30} height={30} />
+                        <span className="menu-bar__icon-label">OCTO-PLAY</span>
                     </div>
                     <div className="menu-bar__content">
                         {props.children && props.children}
