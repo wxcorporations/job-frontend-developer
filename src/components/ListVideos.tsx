4a57493d080d41faf452a218c5972271c6b8142a
fernando j <fer.j.doc@gmail.com>
2025-10-01 18:23:51 -0300

    feat: setando dados no video na store
    - setando video selecinado
    - setando video nos favoritos

--- src/components/ListVideos.tsx
+++ src/components/ListVideos.tsx
@@ -1,25 +1,28 @@
 import responseVideos from '../mock/videos.json'
 import DataVideo from '../core/DataVideo'
 import CardVideo from './CardVideo'
+import { useDispatch, useSelector } from 'react-redux'
+import { addFavorite, play } from '../../store/videoSlice'
 
 import './ListVideos.scss'
 
-
 export default function ListVideos(props: any): React.ReactNode {
+    const dispath = useDispatch()
+
     const cards = responseVideos.items
-    .map((data) => new DataVideo(data))
-    .map((data, index) => {
-        return (
-            <>
-                <CardVideo 
-                    key={index}
-                    play={(data: any) => console.log('play ========> ', data)}
-                    handleFavorite={(data: any) => console.log('favorite ========> ', data)}
-                    data={data} 
-                />
-            </>
-        )
-    })
+        .map((data) => new DataVideo(data))
+        .map((data, index) => {
+            return (
+                <>
+                    <CardVideo
+                        key={index}
+                        play={(data: any) => dispath(play(data))}
+                        handleFavorite={(data: any) => dispath(addFavorite(data))}
+                        data={data}
+                    />
+                </>
+            )
+        })
 
     return (
         <>
