d0e4a7e4e563092e27433a8cea4af1eeeabcf21d
fernando j <fer.j.doc@gmail.com>
2025-10-03 16:33:18 -0300

    feat: integrando busca google ao input do header

--- src/components/layout/MenuBar.tsx
+++ src/components/layout/MenuBar.tsx
@@ -2,15 +2,17 @@ import React from "react"
 import InputSearch from "../InputSearch"
 
 import useVideo from "../../hooks/useVideo"
+import useYoutubeSearch from "../../hooks/useYoutubeSearch"
 
 import './MenuBar.scss'
 
-type props = {
-    children?: React.ReactNode
-}
 
-export default function MenuBar(props: props) {
-    const { setPlayer, list } = useVideo()
+export default function MenuBar(props: {
+    children?: React.ReactNode
+}) {
+    const { searchVideos } = useYoutubeSearch()
+    const { list } = useVideo()
+    
     const isActive = list.length > 0
 
     return (
@@ -18,11 +20,18 @@ export default function MenuBar(props: props) {
             <header className="menu-bar">
                 <div className="container">
                     <div className="menu-bar__icon">
-                        <img className="menu-bar__icon-img" src="/assets/octocat.png" alt="imagem de um gato com tentaculos de polvo segurando o icone do youtube" width={30} height={30} />
+                        <img 
+                            className="menu-bar__icon-img"
+                            src="/assets/octocat.png"
+                            alt="imagem de um gato com tentaculos de polvo segurando o icone do youtube"
+                            width={30}
+                            height={30} 
+                        />
+
                         <span className="menu-bar__icon-label">OCTO-PLAY</span>
                     </div>
                     <div className="menu-bar__content">
-                        {isActive && <InputSearch search={setPlayer} />}
+                        {isActive && <InputSearch search={searchVideos} />}
                     </div>
                 </div>
             </header>
