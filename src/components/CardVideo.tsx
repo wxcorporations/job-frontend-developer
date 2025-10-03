37e9948e7d5cb8519e1f3e9e914832abdcf34d61
fernando j <fer.j.doc@gmail.com>
2025-10-03 16:17:57 -0300

    feat: utilizando custom hook
    - atualizado strutura de dados
    - utilizando hooks de integracao com store

--- src/components/CardVideo.tsx
+++ src/components/CardVideo.tsx
@@ -1,61 +1,45 @@
 import { useState } from 'react';
-import { Video } from '../interfaces/Video';
 
 import './CardVideo.scss'
 
-
-type DataCard = {
-    data: Video,
-    handleFavorite?: Function,
-    play?: Function
-}
-
-
-function factoryDataVideo(data: Video) {
-    return {
-        id: data.getVideoId(),
-        title: data.getTitle(),
-        description: data.getDescription(),
-        thumb: data.getThumb(),
-        channel: data.getChannel()
-    }
-}
-
 const TOTAL_CHAR_TITLE = 32
 
-export default function CardVideo(props: DataCard) {
+export default function CardVideo(props: {
+    data: any,
+    handleFavorite?: Function,
+    play?: Function
+}) {
     const [isFavorite, setIsFavorite] = useState(false)
-    const DATA_VIDEO: any = factoryDataVideo(props.data)
 
 
     const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
         event.stopPropagation();
-        props.play && props.play(DATA_VIDEO)
+        props.play && props.play(props.data)
     }
 
     const toggleFavorite = (event: React.MouseEvent<HTMLDivElement>) => {
         event.stopPropagation();
         const status = !isFavorite
 
-        props.handleFavorite && (props.handleFavorite({ status, ...DATA_VIDEO }))
+        props.handleFavorite && (props.handleFavorite({ status, ...props.data }))
 
         setIsFavorite(status)
     }
 
     return (
         <>
-            <div role="card" id={props.data.getVideoId()} className='card-video' onClick={handleClick}>
+            <div role="card" id={props.data.id} className='card-video' onClick={handleClick}>
                 <div className='card-video__img'>
                     <div className={`card-video__img-action ${isFavorite ? 'is-active' : ''}`} onClick={toggleFavorite} >
                         <i className="bi bi-heart-fill"></i>
                     </div>
-                    <img className='' src={props.data.getThumb()} title={props.data.getImgTitle()} />
+                    <img className='' src={props.data.thumbnail} title={props.data.title} />
                 </div>
 
                 <div className='card-video__content'>
                     <div className='card-video__content-info'>
-                        <h2 className='title'>{props.data.getTitle().substring(0, TOTAL_CHAR_TITLE).concat('...')}</h2>
-                        <p className='channel'>{props.data.getChannel()}</p>
+                        <h2 className='title'>{props.data.title.substring(0, TOTAL_CHAR_TITLE).concat('...')}</h2>
+                        <p className='channel'>{props.data.channel}</p>
                     </div>
                 </div>
             </div>
