04aafcfd95ba7273883eb306ed824c11c6b79c7b
fernando j <fer.j.doc@gmail.com>
2025-10-04 07:53:36 -0300

    feat: otimizacao

--- modules/lib/index.ts
+++
@@ -1,3 +0,0 @@
-export default function vendor() {
-    console.log("lib carregador!")
-}
--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,32 @@ const Dotenv = require('dotenv-webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: {
         app: './index.tsx',
-        vendor: './modules/vendor/index.ts',
-        libs: './modules/lib/index.ts'
     },
-    mode: 'development',
+    // mode: 'development',
+    mode: 'production',
+    optimization: {
+        concatenateModules: true,
+        removeAvailableModules: true,
+        minimize: true,
+        minimizer: [
+            new TerserPlugin({
+                terserOptions: {
+                    compress: true,
+                    mangle: true,
+                    format: {
+                        comments: false,
+                    },
+                },
+                extractComments: false,
+            }),
+        ],
+    },
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[hash].js',
@@ -41,10 +58,10 @@ module.exports = {
             {
                 test: /\.s[ac]ss$/i,
                 use: [
-                    'style-loader',
+                    MiniCssExtractPlugin.loader,
                     'css-loader',
-                    'sass-loader'
-                ]
+                    'sass-loader',
+                ],
             },
             {
                 test: /\.jsx?$/,
@@ -54,13 +71,14 @@ module.exports = {
             {
                 test: /\.(gif|png|jpe?g|svg)$/i,
                 type: 'asset/*',
+                exclude: /node_modules/,
                 use: [
                     'file-loader',
                     {
                         loader: 'image-webpack-loader',
                         options: {
-                            bypassOnDebug: true, // webpack@1.x
-                            disable: true, // webpack@2.x and newer
+                            bypassOnDebug: true,
+                            disable: true,
                         },
                     },
                 ],
@@ -71,10 +89,11 @@ module.exports = {
         new Dotenv(),
         new HtmlWebpackPlugin({ template: './src/index.html' }),
         new ReactRefreshWebpackPlugin(),
-         new BundleAnalyzerPlugin({
+        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' }),
+        new BundleAnalyzerPlugin({
             analyzerMode: 'static',
             reportFilename: 'report.html',
             openAnalyzer: false,
-        })
+        }),
     ]
 };
