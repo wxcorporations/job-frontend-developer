8d35b1700f1531ce667f992127403c412d12d53f
fernando j <fer.j.doc@gmail.com>
2025-10-04 13:14:17 -0300

    feat: removendo possiveis duplicidades;

--- src/hooks/useFavorites.ts
+++ src/hooks/useFavorites.ts
@@ -6,11 +6,16 @@ export default function useFavorites() {
 
     const list = useSelector((store:any) => store.favorites.items)
 
+    const hasDuplicate = (value:any) => {
+        return list.find((item:any) => item.id === value.id) || false
+    }
+
     return {
         addFavorite: (data: any) => { 
+            if (hasDuplicate(data)) return 
+
             console.log('userFavorite add => ', data)    
             dispatch(addFavorite(data)) 
-        
         },
         removeFavorite: (id: string) => { dispatch(removeFavorite(id)) },
         list 
