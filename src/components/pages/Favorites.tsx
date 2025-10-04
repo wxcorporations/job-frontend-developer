7a31843cea40c37fa11cb0846ce8e0ee278d4915
fernando j <fer.j.doc@gmail.com>
2025-10-04 15:10:35 -0300

    feat: versao inicial da pagina de favoritos

--- src/components/pages/Favorites.tsx
+++ src/components/pages/Favorites.tsx
@@ -1,9 +1,21 @@
-import ListVideos from "../ListVideos";
-
+import useFavorites from "../../hooks/useFavorites";
+import CardVideo from "../CardVideo";
 export default function Favorites(props: any) {
+    const { list, addFavorite, removeFavorite } = useFavorites()
+
+    const toggleFavorite = (data:any) => {
+        data.status ? (addFavorite(data)) : (removeFavorite(data))
+    }
+
+    const cards = list.length 
+        ? list.map((data:any) => {
+            return <CardVideo data={data} handleFavorite={toggleFavorite}/>
+        })
+        : []
+
     return (
         <>
-            <h1>favoritos</h1>
+            {cards}
         </>
     )
 }
