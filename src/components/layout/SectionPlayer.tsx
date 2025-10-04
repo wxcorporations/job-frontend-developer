875ce14db197867b03050eece6f6594db60b9cae
fernando j <fer.j.doc@gmail.com>
2025-10-04 13:13:28 -0300

    feat: identificar itens da lista de videos favoritados
    - caso o video retornado na busca já esteja favoritado
      o icone heart ja vem preenchido e vermelho.

--- src/components/layout/SectionPlayer.tsx
+++ src/components/layout/SectionPlayer.tsx
@@ -2,12 +2,21 @@ import YoutubeEmbed from '../YoutubeEmbed'
 import ListVideos from '../ListVideos'
 
 import useVideo from '../../hooks/useVideo'
+import useFavorites from '../../hooks/useFavorites'
 
 import './SectionPlayer.scss'
 
+
+
 export default function SectionPlayer(props: any) {
+    const { list: listFavorites } = useFavorites()
     const { player, list } = useVideo()
 
+    const listUpdated = list.map((video: any) => {
+            const favorite = listFavorites.find((fav: any) => fav.id === video.id)
+            return favorite ? favorite : video
+        })
+        
     return (
         <>
             <div className="section-player">
@@ -15,13 +24,13 @@ export default function SectionPlayer(props: any) {
                     <YoutubeEmbed id={player?.id || list[0].id} />
 
                     <div className='p-2'>
-                        <h1 className='mt-2'>{player?.title || list[0].title }</h1>
-                        <p className='mb-2'>{player?.channel || list[0].channel }</p>
-                        <p className='text-secondary'>{player?.description || list[0].description }</p>
+                        <h1 className='mt-2'>{player?.title || list[0].title}</h1>
+                        <p className='mb-2'>{player?.channel || list[0].channel}</p>
+                        <p className='text-secondary'>{player?.description || list[0].description}</p>
                     </div>
                 </div>
                 <div className="section-player__content">
-                    <ListVideos items={list || []} />
+                    <ListVideos items={listUpdated || []} />
                 </div>
             </div>
         </>
