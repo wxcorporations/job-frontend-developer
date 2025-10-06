4f2ffabfc379526e0894aa25f41b03b4098c5c87
fernando j <fer.j.doc@gmail.com>
2025-10-06 14:29:35 -0300

    feat: atualizando store
    - atualizando funcionalidade limpar stado store videoSlice
    - registrando slice Menubar na store

--- store/store.ts
+++ store/store.ts
@@ -1,6 +1,7 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import videoSlice from './videoSlice';
 import favoritesSlice from './favoritesSlice';
+import menuBarSlice from './menuBarSlice';
 import searhSlice from './searchSlice';
 
 
@@ -11,7 +12,8 @@ import storage from 'redux-persist/lib/storage';
 const reducers = combineReducers({
   video: videoSlice,
   favorites: favoritesSlice,
-  search: searhSlice
+  search: searhSlice,
+  menu: menuBarSlice
 })
 
 const persistConfig = {
--- store/videoSlice.ts
+++ store/videoSlice.ts
@@ -8,9 +8,11 @@ const videoSlice = createSlice({
     },
     reducers: {
         setPlayer: (state, action) => { state.player = action.payload },
+        resetPlayer: (state) => { state.player = {} },
         updateVideos: (state, action) => { state.list = action.payload },
+        resetVideos: (state) => { state.list = [] },
     }
 })
 
 export default videoSlice.reducer
-export const { setPlayer, updateVideos } = videoSlice.actions
+export const { setPlayer, updateVideos, resetPlayer, resetVideos } = videoSlice.actions
