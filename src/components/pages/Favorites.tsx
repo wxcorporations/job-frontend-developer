071eb137ecefcdfb38ae05be61377851281841ad
fernando j <fer.j.doc@gmail.com>
2025-10-06 14:14:54 -0300

    feat: Molhorias componente Favorites
    - adicao template 0 favoritos.
    - atualização do controle de exibicao do campo de busca.
    - aplicando memoizacao em lista

--- src/components/pages/Favorites.tsx
+++ src/components/pages/Favorites.tsx
@@ -1,21 +1,54 @@
+import { useEffect, useMemo } from "react";
+
+import useStoreMenuBar from "../../hooks/useStoreMenuBar";
 import useFavorites from "../../hooks/useFavorites";
+
 import CardVideo from "../CardVideo";
+
+
 export default function Favorites(props: any) {
+    const { switchSearch } = useStoreMenuBar()
     const { list, addFavorite, removeFavorite } = useFavorites()
 
-    const toggleFavorite = (data:any) => {
+    useEffect(() => {
+        switchSearch(false)
+    }, [])
+
+
+    const toggleFavorite = (data: any) => {
         data.status ? (addFavorite(data)) : (removeFavorite(data))
     }
 
-    const cards = list.length 
-        ? list.map((data:any) => {
-            return <CardVideo data={data} handleFavorite={toggleFavorite}/>
-        })
-        : []
+    const cards = useMemo(() => {
+        return list.length
+            ? list.map((data: any, index: number) => {
+                return <CardVideo key={`card-${index}`} data={data} handleFavorite={toggleFavorite} />
+            })
+            : []
+
+    }, [list])
+
+    const templateFavoriteNone = () => {
+        return (
+            <>
+                <div className="w-100 vh-75 d-flex flex-column justify-content-center align-items-center">
+                    <img
+                        src="/assets/octocat-zero.png"
+                        alt="icon do octocat triste, por não ter favoritos"
+                        width={256}
+                        height={256}
+                    />
+                    <h2 className="mt-5 text-secondary">
+                        0 favorito :(
+                    </h2>
+                </div>
+            </>
+        )
+    }
 
     return (
         <>
-            {cards}
+            {cards.length ? cards : templateFavoriteNone()}
         </>
     )
 }
