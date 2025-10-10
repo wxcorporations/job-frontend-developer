bb48c60e4eb2f4e823b0a20017c0336cd3985793
fernando j <fer.j.doc@gmail.com>
2025-10-10 00:14:31 -0300

    fix: ajuste de lint remocao de codigo morto

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
+import React from "react"
 import FooterBar from "./components/layout/FooterBar"
 import Main from "./components/layout/Main"
 import MenuBar from "./components/layout/MenuBar"
 import { Outlet } from "react-router"
 
 export default function App() {
-  return (<>
-    {/* <Provider store={store}> */}
-      <MenuBar/>
+  return (
+    <>
+      <MenuBar />
       <Main>
         <Outlet />
       </Main>
       <FooterBar />
-    {/* </Provider> */}
-  </>)
+    </>)
 };
