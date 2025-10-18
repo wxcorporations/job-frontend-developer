9a7bbb259899cec915a0b131c7d2de9154ad2669
fernando j <fer.j.doc@gmail.com>
2025-10-18 14:19:49 -0300

    feat: aplicando code split

--- index.tsx
+++ index.tsx
@@ -1,3 +1,4 @@
+import React, { Suspense, lazy } from 'react'
 import { PersistGate } from 'redux-persist/integration/react';
 import { store, persistor } from './store/store';
 import { Provider } from 'react-redux';
@@ -5,9 +6,9 @@ import { Provider } from 'react-redux';
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
 
-import Favorites from "./src/components/pages/Favorites";
-import About from "./src/components/pages/About";
-import Home from "./src/components/pages/Home";
+const Favorites = lazy(() => import("./src/components/pages/Favorites"))
+const About = lazy(() => import("./src/components/pages/About"))
+const Home = lazy(() => import("./src/components/pages/Home"))
 
 import PageTop from "./src/components/layout/PageTop";
 import App from "./src/App";
@@ -17,19 +18,23 @@ import './src/sass/index.scss'
 const pageTop = PageTop()
 pageTop.init()
 
+
+
 const root = createRoot(document.getElementById('root')!);
 root.render(
     <Provider store={store}>
+                        <Suspense fallback={<div>Carregando...</div>}>
         <PersistGate loading={null} persistor={persistor}>
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<App />}>
-                        <Route index element={<Home />} />
-                        <Route path="favorites" element={<Favorites />} />
-                        <Route path="about" element={<About />} />
+                            <Route index element={<Home />} />
+                            <Route path="favorites" element={<Favorites />} />
+                            <Route path="about" element={<About />} />
                     </Route>
                 </Routes>
             </BrowserRouter>
         </PersistGate>
+                        </Suspense>
     </Provider>
 );
