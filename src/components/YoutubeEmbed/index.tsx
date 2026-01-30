9110510aa2ca5c4d2ac70ab07c9e2370a463c78f
fernando j <fer.j.doc@gmail.com>
2026-01-30 16:41:55 -0300

    feat: add de placeholder do player

--- src/components/YoutubeEmbed/index.tsx
+++ src/components/YoutubeEmbed/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { PlayBtnFill } from "react-bootstrap-icons"
 
 import './index.scss'
 
@@ -7,14 +8,28 @@ type YoutubeEmbedProps = {
 }
 
 export default function YoutubeEmbed({ id }: YoutubeEmbedProps) {
+    const [load, setLoad] = useState(false)
+    
     return (
         <div className="youtube-embed">
+                
+            <div className='youtube-embed__fallback'
+                style={{ visibility: !load ? 'visible' : 'hidden' }}
+            >
+                <PlayBtnFill />
+            </div>
+        
             <iframe
+                style={{ visibility: !load ? 'hidden': 'visible' }}
+                title='player do youtube'
                 frameBorder="0"
                 allowFullScreen
+                loading='lazy'
+                onLoad={() => setLoad(true)}
                 src={`https://www.youtube.com/embed/${id}`}
             >
             </iframe>
+            
         </div>
     )
 }
